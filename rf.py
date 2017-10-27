3accf8dd877151f976ca53754a63ed5998db7543
gold6 <adam.goldsher@gmail.com>
2017-10-27 17:31:15 -0500

    added gbm and comparison option

--- rf.py
+++ rf.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 #from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_curve
@@ -9,14 +10,14 @@ import pylab as pl
 import numpy as np
 
 
-def RandomForest(data, i):
+def RandomForest(data, i, horizon, itr):
 	
-	horizon = int(raw_input("What is the horizon we want to predict in months? "))
+	#horizon = int(raw_input("What is the horizon we want to predict in months? "))
 		
-	f = open("Output.txt","w")
+	f = open("RF_Results.txt","w")
 	
 	count = 0
-	itr = int(raw_input("How many times should we iterate? "))
+	#itr = int(raw_input("How many times should we iterate? "))
 	while count < itr: 
 		
 		X_train, y_train, X_test, y_test = handlecsv(data, count, horizon, i)
@@ -28,6 +29,32 @@ def RandomForest(data, i):
 		y_predict = rf.predict(X_test)
 
 		y_prob = rf.predict_proba(X_test)
+
+		computestats(y_predict, y_prob, count, y_test, f)
+		
+		count += 1
+
+	f.close()
+
+def GBM(data, i, horizon, itr):
+
+	#horizon = int(raw_input("What is the horizon we want to predict in months? "))
+		
+	f = open("GBM_Results.txt","w")
+	
+	count = 0
+	#itr = int(raw_input("How many times should we iterate? "))
+	while count < itr: 
+		
+		X_train, y_train, X_test, y_test = handlecsv(data, count, horizon, i)
+
+		gbm = GradientBoostingClassifier(n_estimators = 30, max_depth = 15, max_features=12, learning_rate = 0.1)
+
+		gbm.fit(X_train, y_train)
+
+		y_predict = gbm.predict(X_test)
+
+		y_prob = gbm.predict_proba(X_test)
 
 		computestats(y_predict, y_prob, count, y_test, f)
 		
@@ -82,9 +109,16 @@ def main():
 	data = raw_input("Name the data .csv file: ")
 	#data2 = raw_input("Name the data file that want to predict: ")
 	i = raw_input("How much test data should we use (in years): ")
-	model = raw_input("What kind of model would you like to use?" + "\n" + "(1) Random Forest" + "\n")
+	model = raw_input("What kind of model would you like to use?" + "\n" + "(1) Random Forest" + "\n" + "(2) GBM" + "\n" + "(3) Compare all methods" + "\n")
+	horizon = int(raw_input("What is the horizon we want to predict in months? "))
+	itr = int(raw_input("How many times should we iterate? "))
 	if model=='1':
-		RandomForest(data, i)
+		RandomForest(data, i, horizon, itr)
+	if model=='2':
+		GBM(data, i, horizon, itr)
+	if model=='3':
+		RandomForest(data, i, horizon, itr)
+		GBM(data, i, horizon, itr)
 
 if __name__ == "__main__":
 	main()
